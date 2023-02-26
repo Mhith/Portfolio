aba457ce5c89ed613011af08de1f7ad56e7320d6
Mhith <rst.mhith@gmail.com>
2023-02-27 00:39:44 +0530

    Updated links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -93,7 +93,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href=""
+                href="https://github.com/Mhith/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
